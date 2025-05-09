1d4a94207ef5f988ac0f33313d7c89bcdf159c83
Ender-user <tear36.mc@gmail.com>
2025-05-10 00:54:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ available_versions = [v['id'] for v in get_available_versions(game_directory) if
 def check_for_update():
     try:
         version_url = "https://tonsiteweb.com/launcher/version.txt"
-        script_url = "https://tonsiteweb.com/launcher/main.py"
+        script_url = "https://raw.githubusercontent.com/Ender-user/amclauncher/refs/heads/main/main.py"
 
         latest_version = urllib.request.urlopen(version_url).read().decode("utf-8").strip()
 
